35e9b1967e42c5fd144a26eb8482dd0974c5fa5e
Helsi <helsi_@hotmail.com>
2025-02-28 11:42:26 -0400

    adding files to repo

---
+++ src/data/items.ts
@@ -0,0 +1,15 @@
+import android from '../svgs/android.svg'
+import caminhao from '../svgs/caminhao.svg'
+import disney from '../svgs/disney.svg'
+import estrela from '../svgs/estrela.svg'
+import gasolina from '../svgs/gasolina.svg'
+import moto from '../svgs/moto.svg'
+
+export const items = [
+    { name: 'android', image: android},
+    { name: 'caminhao', image: caminhao},
+    { name: 'disney', image: disney},
+    { name: 'estrela', image: estrela},
+    { name: 'gasolina', image: gasolina},
+    { name: 'moto', image: moto}
+]
